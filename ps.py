691915507580990db6a4d99d5e73c35f259524aa
sudheerjampana <sudheerjampanaoffl@gmail.com>
2025-05-05 16:20:32 +0530

    modified and new ps file added

--- ps.py
+++ ps.py
@@ -16,17 +16,17 @@
 #         res+=chr(count)+" "
 #         count+=1
 #     print(res)
-# rows=5
-# count=97
-# for i in range(1,rows+1):
-#     res=""
-#     for j in range(1,i+1):
-#         if i==j or i==rows or j==1:
-#             res+=chr(count)+" "
-#         else:
-#             res+=" "+" "
-#         count+=1
-#     print(res)
+rows=5
+count=97
+for i in range(1,rows+1):
+    res=""
+    for j in range(1,i+1):
+        if i==j or i==rows or j==1:
+            res+=chr(count)+" "
+        else:
+            res+=" "+" "
+        count+=1
+    print(res)
 
 """
 1
