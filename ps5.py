6f2ed171092b271d54f711351e72f05fe20f1994
sudheerjampana <sudheerjampanaoffl@gmail.com>
2025-05-05 17:02:02 +0530

    new

--- ps5.py
+++ ps5.py
@@ -1,11 +1,50 @@
-rows=int(input("enter the number of rows: "))
-count=97
-for i in range(1,rows+1):
-    res=''
-    for j in range(1,i+1):
-        if i==j or i==rows or j==1:
-            res+=chr(count)+" "
-        else:
-            res+=" "+" "
-        count+=1
-    print(res)
+"""a 
+b c
+d   f
+g     j
+k l m n o
+"""
+# rows=int(input("enter the number of rows: "))
+# count=97
+# for i in range(1,rows+1):
+#     res=''
+#     for j in range(1,i+1):
+#         if i==j or i==rows or j==1:
+#             res+=chr(count)+" "
+#         else:
+#             res+=" "+" "
+#         count+=1
+#     print(res)
+"""
+        *
+      * *
+    * * *
+  * * * *
+* * * * *
+"""
+# rows=5
+# for i in range(1,rows+1):
+#     res=""
+#     for j in range(1,rows+1):
+#         if i+j>=rows+1:
+#             res+="*"+" "
+#         else:
+#             res+=" "+" "
+#     print(res)
+"""
+*
+* *
+* * *
+* * * *
+* * * * *
+"""
+
+# rows=5
+# for i in range(1,rows+1):
+#     res=""
+#     for j in range(1,rows+1):
+#         if i+j<=rows+1:
+#             res+="*"+" "
+#         else:
+#             res+=" "+" "
+#     print(res)
